a02df8cab63367d1a648e74165a371d9e7721e32
Andrés Añez <aeanez@gmail.com>
2021-11-05 17:35:51 -0500

    Adding hack to validate connectivity

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -20,10 +20,14 @@
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener("offline", onOffline, false);
+
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
-
+    
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+    
 }
+
